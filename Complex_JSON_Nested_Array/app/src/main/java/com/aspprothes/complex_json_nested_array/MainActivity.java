da31b318ce66a507c80feb4542689f0ddb0e018c
prothesbarai <aspprothesbarai1619@gmail.com>
2024-11-28 22:55:35 +0600

    Version 1.0

---
+++ Complex_JSON_Nested_Array/app/src/main/java/com/aspprothes/complex_json_nested_array/MainActivity.java
@@ -0,0 +1,14 @@
+package com.aspprothes.complex_json_nested_array;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
