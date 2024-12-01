254a4fb0efa7ee18742a393c304cf962aae071a5
prothesbarai <aspprothesbarai1619@gmail.com>
2024-12-01 22:29:18 +0600

    v2.2

--- Complex_JSON_Nested_Array/app/src/main/java/com/aspprothes/complex_json_nested_array/MainActivity.java
+++ Complex_JSON_Nested_Array/app/src/main/java/com/aspprothes/complex_json_nested_array/MainActivity.java
@@ -3,8 +3,13 @@ package com.aspprothes.complex_json_nested_array;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 public class MainActivity extends AppCompatActivity {
+    private LottieAnimationView animationView;
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +17,9 @@ public class MainActivity extends AppCompatActivity {
         this.getWindow().setNavigationBarColor(getResources().getColor(R.color.sky));
         this.getWindow().setStatusBarColor(getResources().getColor(R.color.sky));
         setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listView);
+        animationView = findViewById(R.id.animationView);
+
     }
 }
